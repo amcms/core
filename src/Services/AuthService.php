08dad0f10a1eb99cb8439f5bcd563790063e2225
ShCoder-1 <sitemart@gmail.com>
2017-09-04 09:56:35 +0300

    fix load auth service

---
+++ src/Services/AuthService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Amcms\Services;
+
+use Amcms\Contracts\Auth as AuthContract;
+
+class AuthService extends Service implements AuthContract
+{
+
+    
+    public function register(array $credential)
+    {
+        //
+    }
+
+    public function check(array $credential)
+    {
+        //
+    }
+
+    public function login(array $credential)
+    {
+        //
+    }
+
+    public function forceAuth(array $credential)
+    {
+        //
+    }
+
+    public function logout(array $credential)
+    {
+        //
+    }
+}
